ef58db662325169f93a5174e6ff01959674fdda4
Oleg Efrem <oleg.efrem@gmail.com>
2023-11-27 22:59:25 +0000

    Replace cartRepo Ref with MapRef

--- src/main/scala/com/shop/repo/CartRepo.scala
+++ src/main/scala/com/shop/repo/CartRepo.scala
@@ -1,13 +1,13 @@
 package com.shop.repo
 
 import cats.MonadThrow
-import cats.effect.Ref
-import cats.effect.std.UUIDGen
+import cats.effect.Concurrent
+import cats.effect.std.{MapRef, UUIDGen}
+import cats.implicits._
 import com.shop.ShopCartError
 import com.shop.config.Config.CartConfig
 import com.shop.model.cart.{Cart, CartId}
-import cats.implicits._
-import com.shop.repo.error.{CartNotFound, ConcurrentCartModification}
+import com.shop.repo.error.{CartNotFound, ConcurrentCartModification, DifferentCartsReplacement}
 
 trait CartRepo[F[_]] {
   def createCart(): F[Cart]
@@ -24,41 +24,47 @@ object error {
 
   case class ConcurrentCartModification(oldCart: Cart, modifiedCart: Cart, newCart: Cart) extends CartRepoError(s"Cart with id: ${oldCart.id} was already modified.")
 
+  case class DifferentCartsReplacement(oldCart: Cart, newCart: Cart) extends CartRepoError(s"Tried to replace cart with id: ${oldCart.id} with cart with id: ${newCart.id}.")
+
 }
 
 object CartRepo {
-  def make[F[_] : MonadThrow : UUIDGen](cartsRef: Ref[F, Map[CartId, Cart]], config: CartConfig): CartRepo[F] =
+  def make[F[_] : MonadThrow : Concurrent : UUIDGen](cartsRef: MapRef[F, CartId, Option[Cart]], config: CartConfig): CartRepo[F] =
     new CartRepo[F] {
       override def createCart(): F[Cart] = {
         for {
           uuid <- UUIDGen.randomUUID
           cart = Cart.empty(CartId(uuid), config.taxRate)
-          _ <- cartsRef.update(_ + (cart.id -> cart))
+          _ <- cartsRef(cart.id).update(_ => cart.some)
         } yield cart
       }
 
-      override def getCart(cartId: CartId): F[Cart] =
+      override def getCart(cartId: CartId): F[Cart] = {
         for {
-          carts <- cartsRef.get
-          cart <- MonadThrow[F].fromOption(carts.get(cartId), CartNotFound(cartId))
-        } yield cart
+          maybeCart <- cartsRef(cartId).get
+          cartOrNotFound <- MonadThrow[F].fromOption(maybeCart, CartNotFound(cartId))
+        } yield cartOrNotFound
+      }
 
       override def replaceCart(oldCart: Cart, newCart: Cart): F[Cart] = {
         def concurrentModificationError(currentCart: Cart) = {
           ConcurrentCartModification(oldCart, currentCart, newCart)
         }
 
+        val cartsIdValidation =
+          if (oldCart.id =!= newCart.id)
+            Left(DifferentCartsReplacement(oldCart, newCart))
+          else
+            Right(())
         for {
-          updated <- cartsRef.tryModify { carts =>
-            carts.get(oldCart.id) match {
-              case Some(currentCart) if currentCart === oldCart =>
-                val updatedCart = carts + (newCart.id -> newCart)
-                (updatedCart, Right(newCart))
-              case Some(currentCart) if currentCart =!= oldCart =>
-                (carts, Left(concurrentModificationError(currentCart)))
-              case None =>
-                (carts, Left(CartNotFound(oldCart.id)))
-            }
+          _ <- MonadThrow[F].fromEither(cartsIdValidation)
+          updated <- cartsRef(oldCart.id).tryModify {
+            case Some(currentCart) if currentCart === oldCart =>
+              (newCart.some, Right(newCart))
+            case Some(currentCart) if currentCart =!= oldCart =>
+              (oldCart.some, Left(concurrentModificationError(currentCart)))
+            case None =>
+              (oldCart.some, Left(CartNotFound(oldCart.id)))
           }
           currentCart <- getCart(oldCart.id)
           updatedResult <- MonadThrow[F].fromOption(updated, concurrentModificationError(currentCart))
--- src/test/scala/com/shop/repo/CartRepoTest.scala
+++ src/test/scala/com/shop/repo/CartRepoTest.scala
@@ -1,41 +1,43 @@
 package com.shop.repo
 
-import cats.effect.{IO, Ref}
+import cats.effect.IO
+import cats.effect.std.MapRef
 import com.shop.TestData._
 import com.shop.config.Config.CartConfig
 import com.shop.generators._
 import com.shop.model.cart.{Cart, CartId}
-import com.shop.repo.error.CartNotFound
+import com.shop.repo.error.{CartNotFound, DifferentCartsReplacement}
 import munit.{CatsEffectSuite, ScalaCheckEffectSuite}
 import org.scalacheck.effect.PropF.forAllF
-import cats.syntax.all._
+import cats.implicits._
 
 import java.util.UUID
 
 class CartRepoTest extends CatsEffectSuite with ScalaCheckEffectSuite {
 
-  private def refAndRepo(carts: Map[CartId, Cart] = Map.empty): IO[(Ref[IO, Map[CartId, Cart]], CartRepo[IO])] = {
+  private def refAndRepo(carts: Map[CartId, Cart] = Map.empty, shardCount: Int = 10): IO[(MapRef[IO, CartId, Option[Cart]], CartRepo[IO])] = {
     for {
-      carts <- Ref.of[IO, Map[CartId, Cart]](carts)
-    } yield (carts, CartRepo.make[IO](carts, CartConfig()))
+      cartsRef <- MapRef.ofShardedImmutableMap[IO, CartId, Cart](shardCount)
+      _ <- carts.map { case (key, value) => cartsRef(key).update(_ => value.some) }.toList.sequence
+    } yield (cartsRef, CartRepo.make[IO](cartsRef, CartConfig()))
 
   }
 
   test("Creates a new empty cart returning it") {
     for {
       (cartsRef, cartsRepo) <- refAndRepo()
-      newCart <- cartsRepo.createCart()
-      refCarts <- cartsRef.get
-    } yield assertEquals(refCarts(newCart.id), newCart)
+      createdCart <- cartsRepo.createCart()
+      refCart <- cartsRef(createdCart.id).get
+    } yield assertEquals(refCart, createdCart.some)
   }
 
   test("Gets an existing cart") {
     forAllF { (cart: Cart) =>
       for {
         (cartsRef, cartsRepo) <- refAndRepo(Map(cart.id -> cart))
-        obtainedCart <- cartsRepo.getCart(cart.id)
-        refCarts <- cartsRef.get
-      } yield assertEquals(refCarts(obtainedCart.id), obtainedCart)
+        createdCart <- cartsRepo.getCart(cart.id)
+        refCart <- cartsRef(createdCart.id).get
+      } yield assertEquals(refCart, createdCart.some)
     }
   }
 
@@ -55,8 +57,21 @@ class CartRepoTest extends CatsEffectSuite with ScalaCheckEffectSuite {
       for {
         (cartsRef, cartsRepo) <- refAndRepo(Map(oldCart.id -> oldCart))
         _ <- cartsRepo.replaceCart(oldCart, cart)
-        refCarts <- cartsRef.get
-      } yield assertEquals(refCarts(cart.id), cart)
+        refCart <- cartsRef(cart.id).get
+      } yield assertEquals(refCart, cart.some)
+    }
+  }
+
+  test("Returns error on different carts modification") {
+    forAllF { (cart: Cart) =>
+      val oldCart = anEmptyCart()
+      interceptMessageIO[DifferentCartsReplacement](s"CTried to replace cart with id: ${oldCart.id} with cart with id: ${cart.id}.") {
+        for {
+          (_, cartsRepo) <- refAndRepo(Map(cart.id -> cart))
+          _ <- cartsRepo.replaceCart(oldCart, cart)
+        } yield ()
+      }
+      assertIO(IO.pure(1), 1)
     }
   }
 
@@ -74,17 +89,18 @@ class CartRepoTest extends CatsEffectSuite with ScalaCheckEffectSuite {
   }
 
 
-  test("Replaces an existing cart on different carts concurrent modification") {
+  test("Replace different carts concurrently") {
     forAllF { (cart: Cart) =>
       def cartId(i: Int): CartId = CartId(UUID.nameUUIDFromBytes(i.toString.getBytes()))
-
-      val carts = (1 to 10).map(i => anEmptyCart(cartId(i)) -> cart.copy(id = cartId(i))).toMap
+      val cartsSize = 100
+      val cartIds = (1 to cartsSize).map(cartId)
+      val carts = cartIds.map(i => anEmptyCart(i) -> cart.copy(id = i)).toMap
       val emptyCarts = carts.keys.map(ec => ec.id -> ec).toMap
       for {
-        (cartsRef, cartsRepo) <- refAndRepo(emptyCarts)
+        (cartsRef, cartsRepo) <- refAndRepo(emptyCarts, shardCount = cartsSize * 3)
         _ <- carts.map { case (empty, nonEmpty) => cartsRepo.replaceCart(empty, nonEmpty) }.toList.parSequence.void
-        refCarts <- cartsRef.get
-      } yield assertEquals(refCarts.size, 100)
+        refCarts <- cartIds.map(cartsRef(_).get).toList.sequence
+      } yield assertEquals(refCarts.size, cartsSize)
     }
   }
 
